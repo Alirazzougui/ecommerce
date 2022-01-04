10ec07eb81c118e965c932adb68cafd2f139e16f
hsoubecommerce <hsoubecommerce@gmail.com>
2022-01-04 14:53:00 +0100

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,73 @@
+var path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin= require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+
+
+module.exports = {
+ 
+    entry:  {
+      app:'./src/index.js'
+    },
+    
+    output: {
+      path: path.join(__dirname, "/dist"),
+      publicPath: '',
+      filename: "main.js"
+    },
+  
+    mode: "development",
+
+    
+    devServer: {
+        contentBase: path.join(__dirname,"/dist"),
+        port: 8201,
+        writeToDisk: true,
+        open:true
+    },
+
+
+    module: {
+        rules: [
+            {
+                test: /\.html$/,
+                use: [
+                {
+                    loader: "html-loader",
+                        options: {
+                            minimize: true,
+                        }
+                }
+                ]
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader, 
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.(png|svg|jpe?g|gif)$/,
+                use: [
+                  {
+                    loader: "file-loader", 
+                    options: {
+                      name: '[name].[ext]',
+                      outputPath: "images",
+                    }
+                  }
+                ]
+              },
+        ]
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: "index.html",
+            template: "./src/index.html",
+          }),
+        new MiniCssExtractPlugin({filename: "css/style.css"}),
+
+    ],
+}
